4b340b7465922849c80c47b9dde224517650f31a
kazukazu2027 <kazu2000027@gmail.com>
2021-04-06 15:40:35 +0900

    menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import NavMenu from "./NavMenu";
 import Link from 'next/link';
-import SpMenu from './SpMenu';
+import SpMenu from '../Menus/SpMenu';
+import NavMenu from "../Menus/NavMenu";
 
 
 const Header = () => {
